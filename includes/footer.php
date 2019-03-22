20d6ec9470d15452d110dc53df9bba232dec10b4
Andrew Mason <andrewmasonmedia@gmail.com>
2019-03-22 14:52:56 -0400

    Readded .php extensions, changed footer col-md to col-sm-6 for better breaking on ipad

--- includes/footer.php
+++ includes/footer.php
@@ -3,40 +3,40 @@
             <div class="container-fluid p-0 relative stagger-down">
                 <div class="row d-flex d-flex justify-content-center align-items-end text-secondary bg-light text-secondary px-3 pt-5">
                     
-                    <div class="col-lg-3 col-md-6 px-4 py-3 skew down">
+                    <div class="col-lg-3 col-sm-6 px-4 py-3 skew down">
                         <ul class="list-unstyled w-100 slide-down">
                             <li class="h4 font-weight-bold">Jaxx Liberty</li>
                             <hr class="hr-big border-0 bg-secondary">
-                            <li><a href="/legal/terms-current">Terms of Service</a></li>
-                            <li><a href="/legal/intellectual-property">Intellectual Property</a></li>
-                            <li><a href="/legal/thirdparties-current">Third Party Agreements</a></li>
-                            <li><a href="/changeLog">Changelog</a></li>
+                            <li><a href="/legal/terms-current.php">Terms of Service</a></li>
+                            <li><a href="/legal/intellectual-property.php">Intellectual Property</a></li>
+                            <li><a href="/legal/thirdparties-current.php">Third Party Agreements</a></li>
+                            <li><a href="/changeLog.php">Changelog</a></li>
                         </ul>
                     </div>
                     
-                    <div class="col-lg-3 col-md-6 px-4 py-3 skew down">
+                    <div class="col-lg-3 col-sm-6 px-4 py-3 skew down">
                         <ul class="list-unstyled w-100 slide-down">
                             <li class="h4 font-weight-bold">Company</li>
                             <hr class="hr-big border-0 bg-secondary">
-                            <li><a href="/about">About</a></li>
+                            <li><a href="/about.php">About</a></li>
                             <li><a href="https://support.decentral.ca/hc/en-us" target="_blank" onclick="googleAnalyticsTrigger('Support_Page', 'Footer')">Support</a></li>
                             <li><a href="https://blog.jaxx.io/" target="_blank">Blog</a></li>
                             <li><a href="https://decentral.ca" onclick="googleAnalyticsTrigger('decentral.ca', 'Home_Footer')">Decentral</a></li>
                         </ul>
                     </div>
                     
-                    <div class="col-lg-3 col-md-6 px-4 py-3 skew down">
+                    <div class="col-lg-3 col-sm-6 px-4 py-3 skew down">
                         <ul class="list-unstyled w-100 slide-down">
                             <li class="h4 font-weight-bold">Download</li>
                             <hr class="hr-big border-0 bg-secondary">
                             <li><a href="https://play.google.com/store/apps/details?id=com.liberty.jaxx" target="_blank">Android</a></li>
                             <li><a href="https://itunes.apple.com/us/app/jaxx-liberty/id1435383184?ls=1&mt=8" target="_blank">iOS</a></li>
                             <li><a href="https://chrome.google.com/webstore/detail/jaxx-liberty/cjelfplplebdjjenllpjcblmjkfcffne" target="_blank">Chrome</a></li>
-                            <li><a href="/downloads" onclick="googleAnalyticsTrigger('Downloads_Page', 'Footer')">Desktop</a></li>
+                            <li><a href="/downloads.php" onclick="googleAnalyticsTrigger('Downloads_Page', 'Footer')">Desktop</a></li>
                         </ul>
                     </div>
                     
-                    <div class="col-lg-3 col-md-6 px-4 py-3 skew down">
+                    <div class="col-lg-3 col-sm-6 px-4 py-3 skew down">
                         <ul class="list-unstyled w-100 slide-down">
                             <li class="h4 font-weight-bold">Connect</li>
                             <hr class="hr-big border-0 bg-secondary">
